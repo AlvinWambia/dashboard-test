c6aaf45364155abe3cf4b3cafec511c83e6a516d
AlvinWambia <wambialvin@gmail.com>
2026-05-06 15:16:01 +0300

    AUTH fixes

--- app/auth/callback/route.js
+++ app/auth/callback/route.js
@@ -1,16 +1,19 @@
 // app/auth/callback/route.js
 import { NextResponse } from 'next/server'
 import { createClient } from '@/supabase/server' // Updated path to your server client
+import { getURL } from '@/lib/getURL'
 
 export async function GET(request) {
     const { searchParams, origin } = new URL(request.url)
     const code = searchParams.get('code')
+    // Extract the "next" parameter or default to /dashboard
+    const next = searchParams.get('next') ?? '/dashboard'
 
     if (code) {
         const supabase = createClient()
         const { error } = await supabase.auth.exchangeCodeForSession(code)
         if (!error) {
-            return NextResponse.redirect(`${origin}/dashboard`)
+            return NextResponse.redirect(getURL(next))
         }
     }
 
